ccfeffea8305c6ccf5e1720b83594817db0ed4de
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2019-04-21 20:33:49 +0300

    modified package name

--- bin/global.js
+++ bin/global.js
@@ -1,3 +1,11 @@
-#!/usr/bin/env node
+#!/usr/local/opt/python/bin/python3.7
 
-const slowloris = require('../lib/index.js')
+# -*- coding: utf-8 -*-
+import re
+import sys
+
+from slowloris import main
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
